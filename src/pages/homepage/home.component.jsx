6c14efba0f1f8952f52393e2e51c9d1303952ac9
Yogesh Patil <officialyogeshp@gmail.com>
2021-10-23 22:27:22 +0530

    Removed unused styles and added call to Directory component

--- src/pages/homepage/home.component.jsx
+++ src/pages/homepage/home.component.jsx
@@ -1,42 +1,12 @@
 import React from "react";
+import Directory from "../../components/directory/directory.component";
 
 import "./home.styles.scss";
 
 const Home = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMEN</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MEN</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
