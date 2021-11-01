d540b8568b86381cf7610a8403f8f56f8a3a6068
Yogesh Patil <officialyogeshp@gmail.com>
2021-11-01 23:36:03 +0530

    Added basic routing, added higher order component: withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Switch, Route } from "react-router";
+
 import "./App.css";
 import Home from "./pages/homepage/home.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE!</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
