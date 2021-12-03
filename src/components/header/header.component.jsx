86bc1b2100dc4e7ea7b988a514945d081c0ca52e
Yogesh Patil <officialyogeshp@gmail.com>
2021-12-03 23:37:59 +0530

    Added CartIcon component to display Shopping Cart in Header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/images/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 import "./header.styles.scss";
 
@@ -32,6 +33,7 @@ const Header = ({ currentUser }) => {
             SING IN{" "}
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
