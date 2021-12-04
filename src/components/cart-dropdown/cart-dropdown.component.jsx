8fd3328be04cf37e8c4dc3819fde6ac82d536c32
Yogesh Patil <officialyogeshp@gmail.com>
2021-12-04 15:29:14 +0530

    Improved user flow by hidding cart dropdown when user clicks on Go To Checkout button inside Cart Dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { createStructuredSelector } from "reselect";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/cart-item.component";
@@ -9,7 +10,7 @@ import CustomButton from "../custom-button/custom-button.component";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -24,6 +25,7 @@ const CartDropdown = ({ cartItems, history }) => {
       <CustomButton
         onClick={() => {
           history.push("/checkout");
+          dispatch(toggleCartHidden());
         }}
       >
         GO TO CHECKOUT
