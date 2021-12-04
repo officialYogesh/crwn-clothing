40d38cba2844c7b42661524bab1f1d0fca9c5765
Yogesh Patil <officialyogeshp@gmail.com>
2021-12-04 14:29:28 +0530

    Formatted imports

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
+
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/cart-item.component";
-
 import CustomButton from "../custom-button/custom-button.component";
 
 import "./cart-dropdown.styles.scss";
