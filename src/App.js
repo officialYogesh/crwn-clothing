8a16cfae14184431ee4374aee4b417a9ece13d2a
Yogesh Patil <officialyogeshp@gmail.com>
2021-12-04 14:44:46 +0530

    Updated application to use selectors and createStructuredSelector

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router";
-
 import { connect } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
+import { createStructuredSelector } from "reselect";
 
-import "./App.css";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 import Header from "./components/header/header.component";
 import Home from "./pages/homepage/home.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+
+import "./App.css";
 
 const HatsPage = () => (
   <div>
@@ -68,8 +70,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 
-import { ReactComponent as Logo } from "../../assets/images/crown.svg";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 import { auth } from "../../firebase/firebase.utils";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import CartIcon from "../cart-icon/cart-icon.component";
 
+import { ReactComponent as Logo } from "../../assets/images/crown.svg";
 import "./header.styles.scss";
 
 const Header = ({ currentUser, hidden }) => {
@@ -41,9 +44,9 @@ const Header = ({ currentUser, hidden }) => {
   );
 };
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
